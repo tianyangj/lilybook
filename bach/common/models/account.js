b8b7516fbf1d18be9a0c308d016449f956f51634
Tommy Jiang <tianyangj@gmail.com>
2016-02-16 21:25:22 -0800

    disable Account model delete methods

--- bach/common/models/account.js
+++ bach/common/models/account.js
@@ -1,3 +1,5 @@
 module.exports = function(Account) {
-
+    Account.disableRemoteMethod("deleteById", true);
+    Account.disableRemoteMethod('__delete__accessTokens', false);
+    Account.disableRemoteMethod('__destroyById__accessTokens', false);
 };
