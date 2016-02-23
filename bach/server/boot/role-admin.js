3360beb3b0f5cdf012f4fd5e31a6ea7a833b593a
Tommy Jiang <tianyangj@gmail.com>
2016-02-22 21:35:27 -0800

    auto creating admin user

--- bach/server/boot/role-admin.js
+++ bach/server/boot/role-admin.js
@@ -1,21 +1,36 @@
 module.exports = function (app) {
 
+    const ADMIN_ID = 1014;
+
+    var Account = app.models.Account;
     var Role = app.models.Role;
     var RoleMapping = app.models.RoleMapping;
 
-    //create the admin role
-    Role.create({
-        name: 'admin'
-    }).then(function (role) {
-        console.log('Created role:', role);
-        //make tianyangj@gmail.com an admin
-        return role.principals.create({
-            principalType: RoleMapping.USER,
-            principalId: '56c3faa6882b30438c9ca54e'
+    Account.findById(ADMIN_ID).then(function (user) {
+        if (!user) {
+            return Account.create({
+                id: ADMIN_ID,
+                email: 'tianyangj@gmail.com',
+                password: 'qwer1234',
+                admin: true
+            })
+        }
+        return user;
+    }).then(function (user) {
+        console.log('User:', user);
+        return Role.create({
+            name: 'admin'
+        }).then(function (role) {
+            console.log('Created role:', role);
+            return role.principals.create({
+                principalType: RoleMapping.USER,
+                principalId: user.id
+            });
+        }).then(function (principal) {
+            console.log('Created principal:', principal);
         });
-    }).then(function (principal) {
-        console.log('Created principal:', principal);
     }).catch(function (err) {
         console.log('Error:', err);
     });
+
 };
