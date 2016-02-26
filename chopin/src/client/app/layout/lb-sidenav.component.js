59b033e34e4b84d7a96e71cc5a01b57d428ca7a6
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-02-26 00:38:51 -0800

    sidenav changes

--- chopin/src/client/app/layout/lb-sidenav.component.js
+++ chopin/src/client/app/layout/lb-sidenav.component.js
@@ -2,7 +2,17 @@
     'use strict';
 
     angular.module('app.layout').component('lbSidenav', {
-        templateUrl: '/src/client/app/layout/lb-sidenav.html'
+        templateUrl: '/src/client/app/layout/lb-sidenav.html',
+        controller: SidenavComponentController
     });
+
+    SidenavComponentController.$inject = ['$mdSidenav'];
+
+    function SidenavComponentController($mdSidenav) {
+
+        this.close = function () {
+            $mdSidenav('left').close();
+        }
+    }
 
 })();
