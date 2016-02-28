b6320d6608d284d1a5167a8fb05d0fce3448470d
Tommy Jiang <tianyangj@gmail.com>
2016-02-27 22:59:12 -0800

    add composer editor dialog

--- chopin/src/client/app/admin/admin-composers.controller.js
+++ chopin/src/client/app/admin/admin-composers.controller.js
@@ -3,11 +3,54 @@
 
     angular.module('app.admin').controller('AdminComposersController', AdminComposersController);
 
-    AdminComposersController.$inject = ['Composer'];
+    AdminComposersController.$inject = ['$state', '$mdDialog', 'Composer'];
 
-    function AdminComposersController(Composer) {
+    function AdminComposersController($state, $mdDialog, Composer) {
+
+        var self = this;
+
         Composer.find().$promise.then(function (composers) {
-            this.composers = composers;
-        }.bind(this));
+            self.composers = composers;
+        });
+
+        this.gotoComposer = gotoComposer;
+
+        this.openEditor = openEditor;
+
+        function gotoComposer(composer) {
+            console.log('goto')
+            //$state.go('composer', { vanity: composer.id });
+        }
+
+        function openEditor(composer, $event) {
+            $event.stopPropagation();
+            $mdDialog.show({
+                templateUrl: 'app/admin/admin-composers-editor-dialog.html',
+                controller: DialogController,
+                controllerAs: '$ctrl',
+                bindToController: true,
+                locals: { original: composer },
+                targetEvent: $event
+            }).then(function (updated) {
+                console.log('updated composer', updated);
+            });
+        }
+    }
+
+    DialogController.$inject = ['$mdDialog'];
+
+    function DialogController($mdDialog) {
+
+        var self = this;
+
+        this.updated = angular.copy(this.original);
+
+        this.close = function () {
+            $mdDialog.cancel();
+        }
+
+        this.update = function () {
+            $mdDialog.hide(self.updated);
+        }
     }
 })();
