5cd10c7e6b20b5cf82ce5b5d4ce2865088c6f6f9
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-03-07 23:16:17 -0800

    add composition detail

--- chopin/src/client/app/composition/composition.controller.js
+++ chopin/src/client/app/composition/composition.controller.js
@@ -3,11 +3,19 @@
 
     angular.module('app.composition').controller('CompositionController', CompositionController);
 
-    CompositionController.$inject = ['composition']
+    CompositionController.$inject = ['Composition', 'composition']
 
-    function CompositionController(composition) {
+    function CompositionController(Composition, composition) {
 
         this.composition = composition;
+
+        Composition.composer({
+            id: this.composition.id
+        }).$promise.then(function (composer) {
+            this.composer = composer;
+        }.bind(this));
+        
+        //composition.composer()
         console.log(composition)
     }
 })();
