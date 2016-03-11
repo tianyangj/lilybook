5cc4501aacf77b165a6fc189400d1d6be1d60a48
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-03-10 23:11:45 -0800

    add composition like placeholder

---
+++ chopin/src/client/app/composition/composition-like.component.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular.module('app.composition').component('lbCompositionLike', {
+        templateUrl: 'app/composition/composition-like.html',
+        bindings: {
+            composition: '<'
+        },
+        controller: CompositionLikeController
+    });
+
+    function CompositionLikeController() {
+
+        this.like = function () {
+            console.log('todo, like', this.composition);
+        };
+    }
+})();
