04c44b48d6e4a8cfa58f6015d3e0c2abd3dab36c
Tommy Jiang <tianyangj@gmail.com>
2016-03-12 22:25:50 -0800

    Hide some Composer endpoints

--- bach/common/models/composer.js
+++ bach/common/models/composer.js
@@ -1,3 +1,10 @@
 module.exports = function(Composer) {
-
+    Composer.disableRemoteMethod('__create__compositions', false);
+    Composer.disableRemoteMethod('__delete__compositions', false);
+    Composer.disableRemoteMethod('__findById__compositions', false);
+    Composer.disableRemoteMethod('__destroyById__compositions', false);
+    Composer.disableRemoteMethod('__updateById__compositions', false);
+    Composer.disableRemoteMethod('createChangeStream', true);
+    Composer.disableRemoteMethod("updateAll", true);
+    Composer.disableRemoteMethod("count", true);
 };
