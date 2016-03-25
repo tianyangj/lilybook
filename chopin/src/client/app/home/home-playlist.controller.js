0bd047307b7d7626f9bf2dab0d68f0662c75a06f
Tommy Jiang <tianyangj@gmail.com>
2016-03-24 20:52:21 -0700

    get compositions

--- chopin/src/client/app/home/home-playlist.controller.js
+++ chopin/src/client/app/home/home-playlist.controller.js
@@ -3,14 +3,36 @@
 
     angular.module('app.home').controller('HomePlaylistController', HomePlaylistController);
 
-    HomePlaylistController.$inject = ['Account', 'account']
+    HomePlaylistController.$inject = ['Account', 'Composition', 'account']
 
-    function HomePlaylistController(Account, account) {
+    function HomePlaylistController(Account, Composition, account) {
 
         Account.playlists({
             id: account.id
         }).$promise.then(function(playlists) {
             this.playlists = playlists;
+            return playlists.map(function(playlist) {
+                return playlist.compositionId;
+            });
+        }.bind(this)).then(function(compositionIds) {
+            return Composition.find({
+                filter: {
+                    where: {
+                        id: {
+                            inq: compositionIds
+                        }
+                    }
+                }
+            }).$promise.then(function(compositions) {
+                return compositions.reduce(function(prev, curr) {
+                    prev[curr.id] = curr;
+                    return prev;
+                }, {});
+            });
+        }).then(function(compositions) {
+            this.playlists.forEach(function(playlist) {
+                playlist.composition = compositions[playlist.compositionId];
+            });
         }.bind(this));
     }
 
