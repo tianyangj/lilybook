2ee16c4890e56c83e4802febba5427c8052f4ec6
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-03-30 00:54:18 -0700

    composition player

--- chopin/src/client/app/composition/components/composition-player.component.js
+++ chopin/src/client/app/composition/components/composition-player.component.js
@@ -13,16 +13,30 @@
 
     function CompositionPlayerController($scope, $element) {
 
+        var url1 = 'https://soundcloud.com/chieh0326/frederic-chopin-fantaisie';
+        var url2 = 'https://soundcloud.com/mohamed-ibrahim-60/frederic-chopin-nocturne-in-e';
+        var url = url === url1 ? url2 : url1;
+
         $scope.$watch(function() {
             return this.composition;
         }.bind(this), function(newVal, oldVal) {
             if (newVal !== oldVal) {
-                console.log('composition updated', newVal.title);
+                var iframeElement = $element.find('iframe');
+                var widget = SC.Widget(iframeElement[0]);
+                url = url === url1 ? url2 : url1;
+                console.log('loading...', url, newVal.title, iframeElement[0].src);
+                widget.load(url, {
+                    auto_play: false,
+                    buying: false,
+                    liking: false,
+                    download: false,
+                    sharing: false,
+                    show_artwork: false,
+                    show_comments: false,
+                    show_playcount: false,
+                    show_user: false
+                });
             }
         });
-        
-        //var iframeElement = $element.find('iframe');
-        //var widget = SC.Widget(iframeElement[0]);
-        //console.log(iframeElement, widget)
     }
 })();
