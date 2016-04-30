fccce3b40ee758662013a7498903adc28f8ae49e
Tommy Jiang <tianyangj@gmail.com>
2016-04-29 22:00:10 -0700

    add mozart

---
+++ mozart/src/app/index.module.ts
@@ -0,0 +1,29 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import { config } from './index.config';
+import { routerConfig } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+import { GithubContributor } from '../app/components/githubContributor/githubContributor.service';
+import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { acmeNavbar } from '../app/components/navbar/navbar.directive';
+import { acmeMalarkey } from '../app/components/malarkey/malarkey.directive';
+
+declare var malarkey: any;
+declare var moment: moment.MomentStatic;
+
+module lilybook {
+  'use strict';
+
+  angular.module('lilybook', ['ngAnimate', 'ui.router', 'ngMaterial', 'toastr'])
+    .constant('malarkey', malarkey)
+    .constant('moment', moment)
+    .config(config)
+    .config(routerConfig)
+    .run(runBlock)
+    .service('githubContributor', GithubContributor)
+    .service('webDevTec', WebDevTecService)
+    .controller('MainController', MainController)
+    .directive('acmeNavbar', acmeNavbar)
+    .directive('acmeMalarkey', acmeMalarkey);
+}
