8938adfbf93a45f7a9249f3e7d5ef9f106b0c4e3
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-04-30 21:54:37 -0700

    add splash

--- mozart/src/app/index.route.ts
+++ mozart/src/app/index.route.ts
@@ -1,12 +1,17 @@
 /** @ngInject */
 export function routerConfig($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) {
-  $stateProvider
+  /*$stateProvider
     .state('home', {
       url: '/',
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
-    });
+    });*/
+
+  $stateProvider.state('splash', {
+    url: '/',
+    templateUrl: 'app/splash/splash.html'
+  });
 
   $urlRouterProvider.otherwise('/');
 }
