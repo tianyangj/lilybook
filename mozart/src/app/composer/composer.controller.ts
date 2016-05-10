58215b0db1b086bc8820998df883c3f0ef66d275
Tommy Jiang <tianyangj@gmail.com>
2016-05-09 21:00:53 -0700

    composer view

--- mozart/src/app/composer/composer.controller.ts
+++ mozart/src/app/composer/composer.controller.ts
@@ -1,6 +1,8 @@
 export class ComposerController {
 
     composer: any;
+    additions: any[];
+    populars: any[];
 
     /* @ngInject */
     constructor(
@@ -11,7 +13,28 @@ export class ComposerController {
             id: this.$stateParams.vanity
         }).$promise.then((composer: any) => {
             this.composer = composer;
+            this.composer.hero = '//placehold.it/851x315?text=composer+hero+image';
+
+            Composer.compositions({
+                id: this.composer.id,
+                filter: {
+                    order: 'id DESC',
+                    limit: 5
+                }
+            }).$promise.then(compositions => this.additions = compositions);
+
+            Composer.compositions({
+                id: this.composer.id,
+                filter: {
+                    order: 'id ASC',
+                    limit: 10
+                }
+            }).$promise.then(compositions => this.populars = compositions);
         });
+    }
+
+    play(composition) {
+        console.log('todo: play', composition);
     }
 
 }
