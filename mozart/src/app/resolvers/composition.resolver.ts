d3bd1230206295124a428f52593a55ce56dd9bc1
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-05-30 23:30:26 -0700

    composition data

--- mozart/src/app/resolvers/composition.resolver.ts
+++ mozart/src/app/resolvers/composition.resolver.ts
@@ -1,11 +1,16 @@
 /* @ngInject */
-export function getComposition($stateParams, Composition) {
+export function getComposition($stateParams, $timeout, Composition, firebase) {
 
-    return Composition.detail({
+    /*return Composition.detail({
         id: $stateParams.id
     }).$promise.then((response: any) => {
         var composition = angular.copy(response.composition);
         delete response.composition;
         return angular.extend(composition, response);
+    });*/
+
+    return firebase.database().ref('/compositions/' + $stateParams.id).once('value').then(snapshot => {
+        let composition = snapshot.val();
+        return composition;
     });
 }
