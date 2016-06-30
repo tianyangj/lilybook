0d7b746f9eec580920b3e40926509d38867486e1
Tommy Jiang <tianyangj@gmail.com>
2016-06-30 00:44:12 -0700

    clean up

--- mozart/src/app/app/app.component.ts
+++ mozart/src/app/app/app.component.ts
@@ -1,6 +1,11 @@
 export const AppComponent: angular.IComponentOptions = {
-    templateUrl: 'app/app/app.html',
-    bindings: {
-        henle: '<'
-    }
+    template: `
+        <div layout="column" layout-fill>
+            <lb-header></lb-header>
+            <div flex layout="row">
+                <lb-sidenav></lb-sidenav>
+                <ui-view flex></ui-view>
+            </div>
+        </div> 
+    `
 };
--- mozart/src/app/modals/index.ts
+++
@@ -1,9 +0,0 @@
-import { LoginModal } from './login.service';
-import { SignupModal } from './signup.service';
-
-export function registerModals() {
-    angular.module('lilybook')
-        .service('loginModal', LoginModal)
-        .service('signupModal', SignupModal);
-}
-
