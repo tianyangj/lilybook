2042a17dade7857289c2eb2613e9fe7e8eb5f1fe
Tommy Jiang <tianyangj@gmail.com>
2016-07-04 00:31:22 -0700

    remove old modules

--- mozart/src/app/components_old/index.ts
+++
@@ -1,8 +0,0 @@
-import { profileHeaderComponent } from './header/profile-header.component';
-import { profileSidenavComponent } from './sidenav/profile-sidenav.component';
-
-export function registerComponents() {
-    angular.module('lilybook')
-        .component('lbProfileHeader', profileHeaderComponent)
-        .component('lbProfileSidenav', profileSidenavComponent);
-}
