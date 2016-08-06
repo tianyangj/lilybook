70d8efa8e05120ea7683eccb5da1f2856be011da
Tommy Jiang <tianyangj@gmail.com>
2016-08-06 00:05:43 -0700

    home-main

--- mozart/src/app/components/home/home-main.component.ts
+++ mozart/src/app/components/home/home-main.component.ts
@@ -3,24 +3,33 @@ import { CompositionDataService } from '../../common/data/composition.service';
 class Controller {
 
     compositions = [];
+    compositions1 = [];
 
     private user;
     private controller;
 
     /* @ngInject */
     constructor(
-        private $firebaseObject,
+        private $firebaseArray,
         private compositionDataService: CompositionDataService
-    ) {
-
-    }
+    ) { }
 
     $onInit() {
-        const likesRef = firebase.database().ref('/user-likes').child(this.user.$id);
-        this.$firebaseObject(likesRef).$loaded().then(likes => {
-            angular.forEach(likes, (value, compositionId) => {
-                this.compositionDataService.get(compositionId).$loaded(composition => {
+        const chopinRef = firebase.database().ref('/composers/chopin/compositions');
+        const chopinQuery = chopinRef.orderByKey().limitToLast(5);
+        this.$firebaseArray(chopinQuery).$loaded().then(compositions => {
+            angular.forEach(compositions, (composition) => {
+                this.compositionDataService.get(composition.$id).$loaded(composition => {
                     this.compositions.push(composition);
+                });
+            });
+        });
+        const beethovenRef = firebase.database().ref('/composers/beethoven/compositions');
+        const beethovenQuery = beethovenRef.orderByKey().limitToLast(5);
+        this.$firebaseArray(beethovenQuery).$loaded().then(compositions => {
+            angular.forEach(compositions, (composition) => {
+                this.compositionDataService.get(composition.$id).$loaded(composition => {
+                    this.compositions1.push(composition);
                 });
             });
         });
