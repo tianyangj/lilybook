87f158c07b4b850fdbb875882e17fe75846a43ca
Tommy Jiang <tianyangj@gmail.com>
2016-08-09 00:47:39 -0700

    home-main

--- mozart/src/app/common/data/composer.service.ts
+++ mozart/src/app/common/data/composer.service.ts
@@ -1,20 +1,42 @@
 export class ComposerDataService {
 
-    private composerRef;
+    private composersRef;
+    private compositionsRef;
 
     /* @ngInject */
     constructor(
         private $firebaseObject,
         private $firebaseArray
     ) {
-        this.composerRef = firebase.database().ref('/composers');
+        this.composersRef = firebase.database().ref('/composers');
+        this.compositionsRef = firebase.database().ref('/compositions');
     }
 
     get(composerId: string) {
-        return this.$firebaseObject(this.composerRef.child(composerId));
+        return this.$firebaseObject(this.composersRef.child(composerId));
     }
 
     getAll() {
-        return this.$firebaseArray(this.composerRef);
+        return this.$firebaseArray(this.composersRef);
+    }
+
+    getFeaturedComposers(limit = 5) {
+        // return items where compositionCount is greater than or equal to 1 with limit
+        const query = this.composersRef.startAt(1).orderByChild('compositionCount').limitToLast(limit);
+        return this.$firebaseArray(query).$loaded().then(composers => {
+            // order by compositionCount descending
+            composers.reverse();
+            // return composers with first 10 compositions
+            return composers.map(composer => {
+                return {
+                    composerId: composer.$id,
+                    name: composer.name,
+                    image: composer.image,
+                    compositions: Object.keys(composer.compositions).slice(0, 10).map(compositionId => {
+                        return this.$firebaseObject(this.compositionsRef.child(compositionId));
+                    })
+                };
+            });
+        });
     }
 }
--- mozart/src/app/components/home/home-main.component.ts
+++ mozart/src/app/components/home/home-main.component.ts
@@ -1,39 +1,19 @@
-import { CompositionDataService } from '../../common/data/composition.service';
+import { ComposerDataService } from '../../common/data/composer.service';
 
 class Controller {
 
-    compositions = [];
-    compositions1 = [];
+    composers;
 
-    private user;
     private controller;
 
     /* @ngInject */
     constructor(
-        private $firebaseArray,
-        private compositionDataService: CompositionDataService
+        private composerDataService: ComposerDataService
     ) { }
 
     $onInit() {
-        const chopinRef = firebase.database().ref('/composers/chopin/compositions');
-        const chopinQuery = chopinRef.orderByKey().limitToLast(5);
-        this.$firebaseArray(chopinQuery).$loaded().then(compositions => {
-            angular.forEach(compositions, (composition) => {
-                this.compositionDataService.get(composition.$id).$loaded(composition => {
-                    this.compositions.push(composition);
-                });
-            });
-        });
-        const beethovenRef = firebase.database().ref('/composers/beethoven/compositions');
-        const beethovenQuery = beethovenRef.orderByKey().limitToLast(5);
-        this.$firebaseArray(beethovenQuery).$loaded().then(compositions => {
-            angular.forEach(compositions, (composition) => {
-                this.compositionDataService.get(composition.$id).$loaded(composition => {
-                    this.compositions1.push(composition);
-                });
-            });
-        });
         this.controller.activeTab = 0;
+        this.composerDataService.getFeaturedComposers().then(composers => this.composers = composers);
     }
 }
 
