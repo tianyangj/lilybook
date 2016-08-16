215f0c7c3718382f95af25d2d12e471e37d7f516
Tommy Jiang <tjiang@weilis-MacBook-Pro.local>
2016-08-16 02:02:14 -0700

    home-collections delete

--- mozart/src/app/components/home/home-collections.component.ts
+++ mozart/src/app/components/home/home-collections.component.ts
@@ -10,7 +10,9 @@ class Controller {
 
     /* @ngInject */
     constructor(
+        private $mdDialog,
         private $firebaseArray,
+        private $firebaseObject,
         private compositionDataService: CompositionDataService
     ) { }
 
@@ -22,10 +24,40 @@ class Controller {
         this.$firebaseArray(this.collectionsRef).$loaded().then(collections => {
             this.collections = collections.map(collection => {
                 return {
-                    id: collection.$id,
+                    $id: collection.$id,
                     name: collection.name,
-                    compositions: this.compositionDataService.getMany(Object.keys(collection.compositions))
+                    compositions: this.compositionDataService.getMany(Object.keys(collection.compositions || {}))
                 };
+            });
+        });
+    }
+
+    removeCollection($event, collection) {
+        const confirm = this.$mdDialog.confirm()
+            .title('Would you like to remove "' + collection.name + '" Collection?')
+            .targetEvent($event)
+            .ok('Remove')
+            .cancel('Cancel')
+            .clickOutsideToClose(true);
+        this.$mdDialog.show(confirm).then(() => {
+            const collectionRef = this.collectionsRef.child(collection.$id);
+            this.$firebaseObject(collectionRef).$remove().then(() => {
+                this.$onInit();
+            });
+        });
+    }
+
+    removeComposition($event, composition, collection) {
+        const confirm = this.$mdDialog.confirm()
+            .title('Would you like to remove "' + composition.title + '" from "' + collection.name + '"?')
+            .targetEvent($event)
+            .ok('Remove')
+            .cancel('Cancel')
+            .clickOutsideToClose(true);
+        this.$mdDialog.show(confirm).then(() => {
+            const compositionRef = this.collectionsRef.child(collection.$id).child('compositions').child(composition.$id);
+            this.$firebaseObject(compositionRef).$remove().then(() => {
+                this.$onInit();
             });
         });
     }
