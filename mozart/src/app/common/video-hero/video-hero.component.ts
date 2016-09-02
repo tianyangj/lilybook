b41d6a50d9fa7a213a13bcc2bdcf5a1832321d15
Tommy Jiang <tianyangj@gmail.com>
2016-09-02 00:53:27 -0700

    video hero support webm

--- mozart/src/app/common/video-hero/video-hero.component.ts
+++ mozart/src/app/common/video-hero/video-hero.component.ts
@@ -1,25 +1,43 @@
 class VideoHeroController {
 
-    videoId: string;
+    videoType: string;
+    youtubeId: string;
+    srcUrl: string;
 
     private hero: any;
 
     /** @ngInject */
     constructor(
         private $window: any,
-        private $timeout: angular.ITimeoutService
-    ) {
-        if (this.hero) {
-            this.videoId = this.hero.substr(this.hero.indexOf('?v=') + 3);
-            $timeout(() => {
-                $window.plyr.setup();
-            });
+        private $timeout: angular.ITimeoutService,
+        private $sce: angular.ISCEService
+    ) { }
+
+    $onInit() {
+        if (angular.isString(this.hero)) {
+            this.videoType = 'youtube';
+            this.youtubeId = this.hero.substr(this.hero.indexOf('?v=') + 3);
+        } else if (this.hero) {
+            this.videoType = 'html5';
+            this.srcUrl = this.$sce.trustAsResourceUrl(this.hero.src);
         }
+        this.$timeout(() => {
+            this.$window.plyr.setup();
+        });
     }
 }
 
 export const VideoHeroComponent = {
-    templateUrl: 'app/common/video-hero/video-hero.html',
+    template: `
+        <div class="plyr" ng-switch="$ctrl.videoType">
+            <div ng-switch-when="youtube">
+                <div data-video-id="{{$ctrl.youtubeId}}" data-type="youtube"></div>
+            </div>
+            <video ng-switch-when="html5" poster="/assets/images/poster.png" controls>
+                <source ng-attr-src="{{$ctrl.srcUrl}}" type="video/webm">
+            </video>
+        </div>
+    `,
     controller: VideoHeroController,
     bindings: {
         hero: '<'
