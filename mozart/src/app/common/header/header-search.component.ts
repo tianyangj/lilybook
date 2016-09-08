2693aa084f04699bec3f9b255b44f896e80d908a
Tommy Jiang <tianyangj@gmail.com>
2016-09-08 00:03:09 -0700

    update header-search

--- mozart/src/app/common/header/header-search.component.ts
+++ mozart/src/app/common/header/header-search.component.ts
@@ -1,5 +1,29 @@
 import { ComposerDataService } from '../data/composer.service';
 
+const template = `
+    <md-menu>
+        <md-button class="md-icon-button" ng-click="$mdOpenMenu($event)">
+            <md-icon class="material-icons">search</md-icon>
+        </md-button>
+        <md-menu-content style="padding:0;min-width:300px;">
+            <md-autocomplete 
+                md-autofocus="true" 
+                md-selected-item="$ctrl.selectedItem" 
+                md-search-text="$ctrl.searchText" 
+                md-items="item in $ctrl.querySearch($ctrl.searchText)"
+                md-item-text="item.fullname" 
+                placeholder="Search">
+                <md-item-template>
+                    <span md-highlight-text="$ctrl.searchText" md-highlight-flags="^i">{{item.fullname}}</span>
+                </md-item-template>
+                <md-not-found>
+                    No results matching "{{$ctrl.searchText}}" were found.
+                </md-not-found>
+            </md-autocomplete>
+        </md-menu-content>
+    </md-menu>
+`;
+
 class Controller {
 
     /** @ngInject */
@@ -17,6 +41,6 @@ class Controller {
 }
 
 export const HeaderSearchComponent = {
-    templateUrl: 'app/common/header/header-search.html',
+    template: template,
     controller: Controller
 };
