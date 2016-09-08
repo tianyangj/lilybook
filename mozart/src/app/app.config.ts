2bddbed59f0451c21cc3ca960533381d84584ce1
Tommy Jiang <tianyangj@gmail.com>
2016-09-07 23:57:52 -0700

    update HeaderComponent

--- mozart/src/app/app.config.ts
+++ mozart/src/app/app.config.ts
@@ -1,8 +1,13 @@
 /** @ngInject */
 export function appConfig(
     $logProvider: angular.ILogProvider,
-    $urlRouterProvider: angular.ui.IUrlRouterProvider
+    $urlRouterProvider: angular.ui.IUrlRouterProvider,
+    $mdThemingProvider: angular.material.IThemingProvider
 ) {
     $logProvider.debugEnabled(false);
     $urlRouterProvider.otherwise('/');
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('pink')
+        .warnPalette('red');
 }
--- mozart/src/app/common/header/header.component.ts
+++ mozart/src/app/common/header/header.component.ts
@@ -1,5 +1,21 @@
 import { AuthenticationService } from '../services/authentication.service';
 
+const template = `
+    <md-toolbar>
+        <div class="md-toolbar-tools">
+            <md-button class="md-icon-button" ng-click="$ctrl.toggleSidenav('left')">
+                <md-icon class="material-icons">menu</md-icon>
+            </md-button>
+            <h2 class="md-padding">
+                <a ui-sref="splash" style="font-weight:bold;">LilyBook</a>
+            </h2>
+            <span flex></span>
+            <lb-header-search></lb-header-search>
+            <lb-header-menu user="$ctrl.user"></lb-header-menu>
+        </div>
+    </md-toolbar>
+`;
+
 class Controller {
 
     user;
@@ -20,6 +36,6 @@ class Controller {
 }
 
 export const HeaderComponent = {
-    templateUrl: 'app/common/header/header.html',
+    template: template,
     controller: Controller
 };
