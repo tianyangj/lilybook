479dca07080be5acd09738337a663dc9f85f486b
Tommy Jiang <tianyangj@gmail.com>
2016-09-12 23:48:08 -0700

    thumbnail-card style

--- mozart/src/app/common/thumbnail/thumbnail-card.component.ts
+++ mozart/src/app/common/thumbnail/thumbnail-card.component.ts
@@ -1,13 +1,27 @@
 const template = `
-    <div style="width:376px;height:220px;position:relative;overflow:hidden;">
-        <md-card layout="row" style="position:absolute;bottom:0;">
-            <div flex="40" style="position:relative;margin:8px;">
-                <img ng-src="{{$ctrl.composition.thumbnail.url}}" style="width:100%;position:absolute;bottom:0;left:0;" />
+    <div layout="row" style="position:relative;margin:8px;">
+        <div flex="40" style="padding:8px;z-index:1;text-align:center;">
+            <a ui-sref="composition({id:$ctrl.composition.$id})">
+                <img 
+                    ng-src="{{$ctrl.composition.thumbnail.url}}" 
+                    src="https://placehold.it/200x300"
+                    style="width:100%;max-width:200px;vertical-align:bottom;border-radius:4px;" />
+            </a>
+        </div>
+        <div flex="60">
+        </div>
+        <md-card layout="row" style="position:absolute;bottom:0;left:0;right:0;margin:0;min-height:140px;">
+            <div flex="40">
             </div>
-            <div flex="60" style="position:relative;margin:8px;">
-                <h4 class="md-title line-clamp-2" style="margin:8px 0;">{{$ctrl.composition.title}}</h4>
-                <h5 class="md-subhead line-clamp-1" style="margin:8px 0;">by {{$ctrl.composition.composerId}}</h5>
-                <p class="md-caption line-clamp-3">{{$ctrl.composition.description}}<p>
+            <div flex="60" style="padding:8px 8px 8px 0;">
+                <a ui-sref="composition({id:$ctrl.composition.$id})" 
+                    class="md-title line-clamp-2" 
+                    style="margin:8px 0;line-height:24px;"
+                    md-colors="{color:'primary-600'}">
+                    {{$ctrl.composition.title}}
+                </a>
+                <h5 class="md-subhead line-clamp-1" style="margin:8px 0;line-height:24px;">by {{$ctrl.composition.composerId}}</h5>
+                <p class="md-caption line-clamp-3" style="line-height:16px;">{{$ctrl.composition.description}}<p>
             </div>
         </md-card>
     </div>
