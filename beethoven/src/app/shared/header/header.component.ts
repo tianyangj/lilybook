c65673d2b7913d154d297dd6a139c441dbff92e6
Tommy Jiang <tianyangj@gmail.com>
2016-09-24 00:58:07 -0700

    add material/icon add hammerjs add MdIconRegistry

--- beethoven/src/app/shared/header/header.component.ts
+++ beethoven/src/app/shared/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
   selector: 'lb-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.less']
+  styleUrls: ['./header.component.less'],
+  viewProviders: [MdIconRegistry]
 })
 export class HeaderComponent implements OnInit {
 
--- beethoven/src/app/shared/shared.module.ts
+++ beethoven/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdIconModule } from '@angular2-material/icon';
 
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        MdToolbarModule
+        MdToolbarModule,
+        MdIconModule
     ],
     declarations: [HeaderComponent],
     exports: [HeaderComponent]
