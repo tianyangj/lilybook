61fa8dd1f04d9d8f9439f68bffe9988d6f377675
Tommy Jiang <tianyangj@gmail.com>
2016-09-24 23:00:41 -0700

    add md-button

--- beethoven/src/app/shared/shared.module.ts
+++ beethoven/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdToolbarModule } from '@angular2-material/toolbar';
 import { MdIconModule } from '@angular2-material/icon';
+import { MdButtonModule } from '@angular2-material/button';
 
 import { HeaderComponent } from './header/header.component';
 
@@ -9,7 +10,8 @@ import { HeaderComponent } from './header/header.component';
     imports: [
         CommonModule,
         MdToolbarModule,
-        MdIconModule
+        MdIconModule,
+        MdButtonModule
     ],
     declarations: [HeaderComponent],
     exports: [HeaderComponent]
