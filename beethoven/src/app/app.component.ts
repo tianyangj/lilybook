1cbba6be37c8d9ed135ec1d4d987d39bf36dd05a
Tommy Jiang <tianyangj@gmail.com>
2016-09-30 23:36:37 -0700

    layout update

--- beethoven/src/app/app.component.ts
+++ beethoven/src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { MdSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
   @ViewChild('sidenav') sidenav: MdSidenav;
-
-  title = 'app works!';
 
   onMenuToggle() {
     this.sidenav.toggle();
