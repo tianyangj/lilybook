560c9cb9cc3955e5cec423e22c0988f517bb943d
Tommy Jiang <tianyangj@gmail.com>
2016-10-02 00:42:04 -0700

    add nav component

--- beethoven/src/app/core/core.module.ts
+++ beethoven/src/app/core/core.module.ts
@@ -2,22 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { MoreComponent } from './more/more.component';
+import { NavComponent } from './nav/nav.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   declarations: [
     HeaderComponent,
     SidenavComponent,
     MoreComponent,
+    NavComponent,
     LoginComponent
   ],
   entryComponents: [
