b94e368ec9f328e22f4618ba4a1372a38ae7715b
Tommy Jiang <tianyangj@gmail.com>
2016-10-02 11:39:44 -0700

    add firebase

--- beethoven/src/app/app.component.ts
+++ beethoven/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { MdSidenav } from '@angular/material/sidenav';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'lb-root',
@@ -9,6 +10,12 @@ import { MdSidenav } from '@angular/material/sidenav';
 export class AppComponent {
 
   @ViewChild('sidenav') sidenav: MdSidenav;
+
+  composers: FirebaseListObservable<any[]>;
+
+  constructor(af: AngularFire) {
+    this.composers = af.database.list('composers');
+  }
 
   onMenuToggle() {
     this.sidenav.toggle();
--- beethoven/src/app/app.module.ts
+++ beethoven/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 import { CoreModule } from './core/core.module';
 import { SplashModule } from './splash/splash.module';
 
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyADuDoy8NEkWG-P21XmTEKoWiCl1nV3eWA',
+  authDomain: 'project-6379245924955471702.firebaseapp.com',
+  databaseURL: 'https://project-6379245924955471702.firebaseio.com',
+  storageBucket: 'project-6379245924955471702.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -16,6 +24,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     MaterialModule.forRoot(),
     RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(firebaseConfig),
     CoreModule,
     SplashModule
   ],
