76b2cdc235ed907dc77308f107df97d6285e5ed3
Tommy Jiang <tianyangj@gmail.com>
2016-10-02 11:53:56 -0700

    move firebase call to splash

--- beethoven/src/app/app.component.ts
+++ beethoven/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { MdSidenav } from '@angular/material/sidenav';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'lb-root',
@@ -11,11 +10,7 @@ export class AppComponent {
 
   @ViewChild('sidenav') sidenav: MdSidenav;
 
-  composers: FirebaseListObservable<any[]>;
-
-  constructor(af: AngularFire) {
-    this.composers = af.database.list('composers');
-  }
+  constructor() { }
 
   onMenuToggle() {
     this.sidenav.toggle();
--- beethoven/src/app/splash/splash.component.ts
+++ beethoven/src/app/splash/splash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'lb-splash',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
 
-  constructor() { }
+  composers: FirebaseListObservable<any[]>;
+
+  constructor(private angularFire: AngularFire) { }
 
   ngOnInit() {
+    this.composers = this.angularFire.database.list('/composers');
   }
 
 }
