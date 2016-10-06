49ae5c7c2a3ee069fc05d7c32162e0308d831248
Tommy Jiang <tianyangj@gmail.com>
2016-10-05 22:40:53 -0700

    lb-app

--- beethoven/src/app/app.component.ts
+++ beethoven/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { MdSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'lb-root',
+  selector: 'lb-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
