a1487cefbd617e1dc7e59e8646af2b8035d675dc
Tommy Jiang <tianyangj@gmail.com>
2016-10-07 21:53:13 -0700

    move splash route

--- beethoven/src/app/app.routes.ts
+++ beethoven/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { SplashComponent } from './splash/splash.component';
 
 export const routes: Routes = [
-    { path: '', component: SplashComponent },
     { path: 'composition', loadChildren: 'app/composition/composition.module#CompositionModule' }
 ]
--- beethoven/src/app/splash/splash.module.ts
+++ beethoven/src/app/splash/splash.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
+import { routes } from './splash.routes';
 import { SplashComponent } from './splash.component';
 
 @NgModule({
   imports: [
+    RouterModule.forChild(routes),
     SharedModule
   ],
   declarations: [
