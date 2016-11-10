2addb905c83c5290f2fd79eeaa295fba3459a749
Tommy Jiang <tianyangj@gmail.com>
2016-11-10 00:35:12 -0800

    npm update

--- beethoven/src/typings.d.ts
+++ beethoven/src/typings.d.ts
@@ -2,6 +2,4 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-/// <reference path="../node_modules/firebase/firebase.d.ts" />
-
 declare var System: any;
