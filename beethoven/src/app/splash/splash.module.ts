3120bebe4efe2971a701f788aacb7cffd7eeec0b
Tommy Jiang <tianyangj@gmail.com>
2016-11-14 23:07:37 -0800

    add splash search

--- beethoven/src/app/splash/splash.module.ts
+++ beethoven/src/app/splash/splash.module.ts
@@ -5,6 +5,7 @@ import { SplashRoutingModule } from './splash-routing.module';
 import { SplashComponent } from './splash.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { HeroComponent } from './hero/hero.component';
   declarations: [
     SplashComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    SearchComponent
   ]
 })
 export class SplashModule { }
