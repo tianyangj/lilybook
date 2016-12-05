dfef82dd1d98e649d9ef3d091cf51167659e47e2
Tommy Jiang <tianyangj@gmail.com>
2016-12-04 23:50:01 -0800

    add composition-sheet

--- beethoven/src/app/composition/composition-sheet/composition-sheet.component.ts
+++ beethoven/src/app/composition/composition-sheet/composition-sheet.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+
+import PhotoSwipe from 'photoswipe/dist/photoswipe';
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
 
 @Component({
-  selector: 'lb-composition-sheet',
-  templateUrl: './composition-sheet.component.html',
-  styleUrls: ['./composition-sheet.component.scss']
+    selector: 'lb-composition-sheet',
+    templateUrl: './composition-sheet.component.html',
+    styleUrls: ['./composition-sheet.component.scss']
 })
 export class CompositionSheetComponent implements OnInit {
 
-  constructor() { }
+    @Input() sheet;
 
-  ngOnInit() {
-  }
+    @ViewChild('pswpEl') pswpEl;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    show() {
+        let pswpElement = this.pswpEl.nativeElement;
+        let items = this.sheet.images.map(image => {
+            return {
+                src: image,
+                w: 2550,
+                h: 3300
+            };
+        });
+        let options = {
+            index: 0
+        };
+        let gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    }
 
 }
