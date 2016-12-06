246fa0e4039605e92cdad191c7b0678fafe45f9a
Tommy Jiang <tianyangj@gmail.com>
2016-12-06 00:19:14 -0800

    add MdSnackBar

--- beethoven/src/app/shared/login/login.component.ts
+++ beethoven/src/app/shared/login/login.component.ts
@@ -4,35 +4,35 @@ import { MdDialogRef } from '@angular/material';
 import { DataService } from '../../core/data.service';
 
 @Component({
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  model = {
-    email: '',
-    password: ''
-  };
-  error: any;
+    model = {
+        email: '',
+        password: ''
+    };
+    error: any;
 
-  constructor(
-    private dataService: DataService,
-    private dialogRef: MdDialogRef<LoginComponent>
-  ) { }
+    constructor(
+        private dataService: DataService,
+        private dialogRef: MdDialogRef<LoginComponent>
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  login() {
-    this.error = null;
-    this.dataService.login({
-      email: this.model.email,
-      password: this.model.password
-    }).then((authState) => {
-      this.dialogRef.close(authState);
-    }, (error) => {
-      this.error = error;
-    });
-  }
+    login() {
+        this.error = null;
+        this.dataService.login({
+            email: this.model.email,
+            password: this.model.password
+        }).then((authState) => {
+            this.dialogRef.close(authState);
+        }, (error) => {
+            this.error = error;
+        });
+    }
 
 }
--- beethoven/src/app/shared/more/more.component.ts
+++ beethoven/src/app/shared/more/more.component.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { MdDialog } from "@angular/material";
+import { MdDialog, MdSnackBar } from "@angular/material";
 import { FirebaseAuthState } from 'angularfire2'
 
 import { LoginComponent } from '../login/login.component';
 import { DataService } from '../../core/data.service';
 
 @Component({
-  selector: 'lb-more',
-  templateUrl: './more.component.html',
-  styleUrls: ['./more.component.scss']
+    selector: 'lb-more',
+    templateUrl: './more.component.html',
+    styleUrls: ['./more.component.scss']
 })
 export class MoreComponent implements OnInit {
 
-  authenticated = false;
+    authenticated = false;
 
-  constructor(
-    private mdDialog: MdDialog,
-    private dataService: DataService
-  ) { }
+    constructor(
+        private mdDialog: MdDialog,
+        private mdSnackBar: MdSnackBar,
+        private dataService: DataService
+    ) { }
 
-  ngOnInit() {
-    this.dataService.auth().subscribe((authState: FirebaseAuthState) => {
-      this.authenticated = !!authState;
-    });
-  }
+    ngOnInit() {
+        this.dataService.auth().subscribe((authState: FirebaseAuthState) => {
+            this.authenticated = !!authState;
+        });
+    }
 
-  login() {
-    this.mdDialog.open(LoginComponent);
-  }
+    login() {
+        this.mdDialog.open(LoginComponent).afterClosed().subscribe(authState => {
+            this.mdSnackBar.open('You are logged in.', 'OK');
+        });
+    }
 
-  logout() {
-    this.dataService.logout();
-  }
+    logout() {
+        this.dataService.logout();
+        this.mdSnackBar.open('You are logged out.', 'OK');
+    }
 
 }
