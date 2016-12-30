6576a2bea7f60c3a3da794da5a50d97e93ee1d33
Tommy Jiang <tianyangj@gmail.com>
2016-12-30 01:10:53 -0800

    swiper data bind

--- beethoven/src/app/book/content/content.component.ts
+++ beethoven/src/app/book/content/content.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/merge';
 
 const Swiper = require('swiper');
 
@@ -7,7 +9,7 @@ const Swiper = require('swiper');
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent implements OnInit, AfterViewInit {
+export class ContentComponent implements OnInit {
 
   @ViewChild('swiperContainer') swiperContainer: ElementRef;
   @ViewChild('swiperPagination') swiperPagination: ElementRef;
@@ -15,19 +17,33 @@ export class ContentComponent implements OnInit, AfterViewInit {
   @ViewChild('swiperNext') swiperNext: ElementRef;
   @Input() book;
   swiper: any;
+  pages = [];
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.book)
-  }
-
-  ngAfterViewInit() {
-    this.swiper = new Swiper(this.swiperContainer.nativeElement, {
-      pagination: this.swiperPagination.nativeElement,
-      nextButton: this.swiperNext.nativeElement,
-      prevButton: this.swiperPrev.nativeElement
-    });
+    Observable
+      .merge(...this.book.compositions)
+      .take(this.book.compositions.length)
+      .subscribe((composition: any) => {
+        // builds pages with composition sheet images
+        this.pages = this.pages.concat(composition.sheet.images);
+      }, (err) => {
+        console.error(err);
+      }, () => {
+        // after all compositions have been fetched, initialize swiper
+        setTimeout(() => {
+          this.swiper = new Swiper(this.swiperContainer.nativeElement, {
+            pagination: this.swiperPagination.nativeElement,
+            nextButton: this.swiperNext.nativeElement,
+            prevButton: this.swiperPrev.nativeElement,
+            preloadImages: false,
+            lazyLoading: true,
+            lazyLoadingInPrevNext: true,
+            lazyLoadingInPrevNextAmount: 1
+          });
+        });
+      });
   }
 
 }
