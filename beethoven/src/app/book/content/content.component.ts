56b6ace89c5c8707b5f893509503a508eb7362f7
Tommy Jiang <tianyangj@gmail.com>
2016-12-31 01:08:39 -0800

    book composition deep link

--- beethoven/src/app/book/content/content.component.ts
+++ beethoven/src/app/book/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/merge';
 
@@ -18,8 +19,11 @@ export class ContentComponent implements OnInit {
   swiper;
   compositions = [];
   pages = [];
+  compositionId: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     Observable
@@ -29,7 +33,7 @@ export class ContentComponent implements OnInit {
         // builds compositions
         this.compositions.push(composition);
         // builds pages with composition sheet images
-        this.pages = this.pages.concat(composition.sheet.images);
+        this.buildPages(composition);
       }, (err) => {
         console.error(err);
       }, () => {
@@ -43,8 +47,32 @@ export class ContentComponent implements OnInit {
             lazyLoadingInPrevNext: true,
             lazyLoadingInPrevNextAmount: 1
           });
+          this.slideToPage();
         });
       });
+    this.route.params.subscribe(params => {
+      this.compositionId = params['composition'];
+      this.slideToPage();
+    });
+  }
+
+  private buildPages(composition) {
+    composition.sheet.images.forEach(image => {
+      this.pages.push({
+        compositionId: composition.$key,
+        image: image
+      });
+    });
+  }
+
+  private slideToPage() {
+    if (this.compositionId && this.swiper) {
+      let pageIndex = this.pages.findIndex(page => {
+        return page.compositionId === this.compositionId;
+      });
+      // need to offset cover page
+      this.swiper.slideTo(pageIndex + 1);
+    }
   }
 
 }
