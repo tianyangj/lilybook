6cc1ce9cbd3ecc715d0a4181611cdbf1ac790834
Tommy Jiang <tianyangj@gmail.com>
2017-01-01 00:29:35 -0800

    book merge header and content, remove sidenav

--- beethoven/src/app/book/book.component.ts
+++ beethoven/src/app/book/book.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MdSidenav } from '@angular/material'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/merge';
+
+const Swiper = require('swiper');
 
 @Component({
-  selector: 'lb-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent implements OnInit {
 
-  @ViewChild(MdSidenav) sidenav: MdSidenav;
+  @ViewChild('swiperContainer') swiperContainer: ElementRef;
+  @ViewChild('swiperPrev') swiperPrev: ElementRef;
+  @ViewChild('swiperNext') swiperNext: ElementRef;
   book;
+  title: string;
+  swiper;
+  compositions = [];
+  pages = [];
 
   constructor(
     private route: ActivatedRoute
@@ -19,11 +27,59 @@ export class BookComponent implements OnInit {
   ngOnInit() {
     this.route.data.do(data => {
       console.log('book', data['book']);
-    }).subscribe(data => this.book = data['book']);
+    }).subscribe(data => {
+      this.book = data['book'];
+      this.title = this.book.name;
+      Observable
+        .merge(...this.book.compositions)
+        .take(this.book.compositions.length)
+        .subscribe((composition: any) => {
+          // builds compositions
+          this.compositions.push(composition);
+          // builds pages with composition sheet images
+          this.buildPages(composition);
+        }, (err) => {
+          console.error(err);
+        }, () => {
+          // after all compositions have been fetched, initialize swiper
+          setTimeout(() => {
+            this.swiper = new Swiper(this.swiperContainer.nativeElement, {
+              nextButton: this.swiperNext.nativeElement,
+              prevButton: this.swiperPrev.nativeElement,
+              hashnav: true,
+              preloadImages: false,
+              lazyLoading: true,
+              lazyLoadingInPrevNext: true,
+              lazyLoadingInPrevNextAmount: 1
+            });
+          });
+        });
+    });
   }
 
-  toggleSidenav() {
-    this.sidenav.toggle();
+  slideTo(composition) {
+    if (this.swiper) {
+      if (composition) {
+        this.title = composition.title;
+        let pageIndex = this.pages.findIndex(page => {
+          return page.compositionId === composition.$key;
+        });
+        // need to offset cover page
+        this.swiper.slideTo(pageIndex + 1);
+      } else {
+        this.title = this.book.name;
+        this.swiper.slideTo(0);
+      }
+    }
+  }
+
+  private buildPages(composition) {
+    composition.sheet.images.forEach(image => {
+      this.pages.push({
+        compositionId: composition.$key,
+        image: image
+      });
+    });
   }
 
 }
