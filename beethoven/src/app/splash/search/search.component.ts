48e4998c1ac9d2b4b97b2fd0f5ef74cf3f6ec090
Tommy Jiang <tianyangj@gmail.com>
2017-01-05 01:27:43 -0800

    add algolia search

--- beethoven/src/app/splash/search/search.component.ts
+++ beethoven/src/app/splash/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+const algoliasearch = require('algoliasearch');
+const autocomplete = require('autocomplete.js');
 
 @Component({
   selector: 'lb-splash-search',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    let client = algoliasearch('NEPY6SGO3D', '33c4e0eddc431647c9732590f2bafd42');
+    let index = client.initIndex('compositions');
+    autocomplete('.sbx-custom__input', { hint: false }, [
+      {
+        source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+        displayKey: 'title',
+        templates: {
+          suggestion: (suggestion) => {
+            return suggestion._highlightResult.title.value;
+          }
+        }
+      }
+    ]).on('autocomplete:selected', (event, suggestion, dataset) => {
+      this.router.navigate(['/composition', suggestion.objectID]);
+    });
   }
 
 }
