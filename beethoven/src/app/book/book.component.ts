81e3450417438b1ea8db6ef322b52c73da42adfc
Tommy Jiang <tianyangj@gmail.com>
2017-01-12 01:49:44 -0800

    fix book

--- beethoven/src/app/book/book.component.ts
+++ beethoven/src/app/book/book.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/merge';
 
 const Swiper = require('swiper');
 
@@ -18,7 +16,6 @@ export class BookComponent implements OnInit {
   title: string;
   composition: any;
   swiper;
-  compositions = [];
   pages = [];
 
   constructor(
@@ -31,40 +28,29 @@ export class BookComponent implements OnInit {
     }).subscribe(data => {
       this.book = data['book'];
       this.title = this.book.name;
-      Observable
-        .merge(...this.book.compositions)
-        .take(this.book.compositions.length)
-        .subscribe((composition: any) => {
-          // builds compositions
-          this.compositions.push(composition);
-          // builds pages with composition sheet images
-          this.buildPages(composition);
-        }, (err) => {
-          console.error(err);
-        }, () => {
-          // after all compositions have been fetched, initialize swiper
-          setTimeout(() => {
-            this.swiper = new Swiper(this.swiperContainer.nativeElement, {
-              nextButton: this.swiperNext.nativeElement,
-              prevButton: this.swiperPrev.nativeElement,
-              hashnav: true,
-              preloadImages: false,
-              lazyLoading: true,
-              lazyLoadingInPrevNext: true,
-              lazyLoadingInPrevNextAmount: 1,
-              onSlideChangeStart: (swiper) => {
-                let page = this.pages[swiper.activeIndex - 1];
-                if (page) {
-                  this.composition = this.compositions.find(composition => composition.$key === page.compositionId);
-                  this.title = this.composition.title;
-                } else {
-                  this.composition = null;
-                  this.title = this.book.name;
-                }
-              }
-            });
-          });
+      this.pages = this.buildPages(this.book.compositions);
+      // initialize swiper
+      setTimeout(() => {
+        this.swiper = new Swiper(this.swiperContainer.nativeElement, {
+          nextButton: this.swiperNext.nativeElement,
+          prevButton: this.swiperPrev.nativeElement,
+          hashnav: true,
+          preloadImages: false,
+          lazyLoading: true,
+          lazyLoadingInPrevNext: true,
+          lazyLoadingInPrevNextAmount: 1,
+          onSlideChangeStart: (swiper) => {
+            let page = this.pages[swiper.activeIndex - 1];
+            if (page) {
+              this.composition = this.book.compositions.find(composition => composition.$key === page.compositionId);
+              this.title = this.composition.title;
+            } else {
+              this.composition = null;
+              this.title = this.book.name;
+            }
+          }
         });
+      });
     });
   }
 
@@ -82,13 +68,17 @@ export class BookComponent implements OnInit {
     }
   }
 
-  private buildPages(composition) {
-    composition.sheet.images.forEach(image => {
-      this.pages.push({
-        compositionId: composition.$key,
-        image: image
+  private buildPages(compositions) {
+    let pages = [];
+    compositions.forEach(composition => {
+      composition.sheet.images.forEach(image => {
+        pages.push({
+          compositionId: composition.$key,
+          image: image
+        });
       });
     });
+    return pages;
   }
 
 }
