e0622c96a72e7a77fa69319eb14a47d48a31405b
Tommy Jiang <tianyangj@gmail.com>
2017-01-19 01:17:39 -0800

    header refactor

--- beethoven/src/app/core/layout-default/layout-default.component.ts
+++ beethoven/src/app/core/layout-default/layout-default.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lb-layout-default',
   templateUrl: './layout-default.component.html',
   styleUrls: ['./layout-default.component.scss']
 })
