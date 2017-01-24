749cb9ec1c8823a9fe5e633955dc745f732944b6
Tommy Jiang <tianyangj@gmail.com>
2017-01-23 23:27:51 -0800

    style update

--- beethoven/src/app/composition/composition.component.ts
+++ beethoven/src/app/composition/composition.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'lb-composition',
   templateUrl: './composition.component.html',
   styleUrls: ['./composition.component.scss']
 })
