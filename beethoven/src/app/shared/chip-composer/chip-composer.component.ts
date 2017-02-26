abce64a800e72e0ee1caddc290220a38c37dc2fc
Tommy Jiang <tianyangj@gmail.com>
2017-02-26 00:06:38 -0800

    chip-composer async

--- beethoven/src/app/shared/chip-composer/chip-composer.component.ts
+++ beethoven/src/app/shared/chip-composer/chip-composer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { FirebaseObjectObservable } from 'angularfire2';
+import { Composer } from '../../core/models';
 import { DataService } from '../../core/data.service';
 
 @Component({
@@ -9,20 +10,25 @@ import { DataService } from '../../core/data.service';
 })
 export class ChipComposerComponent implements OnInit {
 
-  @Input() composerId: string;
+  @Input() composer: FirebaseObjectObservable<Composer>;
   @Input() fullname: boolean;
-
-  composer$;
-  composerLink: boolean;
+  composerId: string;
+  showComposer = false;
+  showComposerLink = false;
 
   constructor(
     private dataService: DataService
   ) { }
 
   ngOnInit() {
-    if (this.composerId) {
-      this.composer$ = this.dataService.getComposer(this.composerId);
-      this.dataService.hasComposerCollection(this.composerId).subscribe(composerLink => this.composerLink = composerLink);
+    if (this.composer) {
+      this.showComposer = true;
+      this.composer.switchMap((composer: Composer) => {
+        this.composerId = composer.$key;
+        return this.dataService.hasComposerCollection(composer.$key);
+      }).subscribe(hasComposerLink => {
+        this.showComposerLink = hasComposerLink;
+      });
     }
   }
 
