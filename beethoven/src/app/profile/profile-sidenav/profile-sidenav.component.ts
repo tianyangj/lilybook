8b499b211fd441730fec258d87ca86b66bff309a
Tommy Jiang <tianyangj@gmail.com>
2017-04-10 23:32:59 -0700

    profile sidenav list

--- beethoven/src/app/profile/profile-sidenav/profile-sidenav.component.ts
+++ beethoven/src/app/profile/profile-sidenav/profile-sidenav.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'lb-profile-sidenav',
   templateUrl: './profile-sidenav.component.html',
-  styleUrls: ['./profile-sidenav.component.scss']
+  styleUrls: ['./profile-sidenav.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileSidenavComponent implements OnInit {
+
+  @Input() user;
+  @Input() collections;
 
   constructor() { }
 
