e7cfd76f9938c8fd56aaf06b50172591cae4d29b
Tommy Jiang <tianyangj@gmail.com>
2017-05-09 23:49:39 -0700

    snackbar

--- beethoven/src/app/signup/signup/signup.component.ts
+++ beethoven/src/app/signup/signup/signup.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFire } from 'angularfire2';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/debounceTime';
-import { DataService } from '../../core/data.service';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -18,43 +16,33 @@ export class SignupComponent implements OnInit {
     private router: Router,
     private angularFire: AngularFire,
     private formBuilder: FormBuilder,
-    private dataService: DataService
+    private snackBar: MdSnackBar
   ) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
+      displayName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      username: ['', [Validators.required], [this.usernameValidator.bind(this)]]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
   signup() {
-    console.log('signup', this.signupForm.value);
     if (this.signupForm.valid) {
       this.angularFire.auth.createUser({
         email: this.signupForm.value.email,
         password: this.signupForm.value.password
       }).then(authState => {
         return authState.auth.updateProfile({
-          displayName: this.signupForm.value.name,
+          displayName: this.signupForm.value.displayName,
           photoURL: ''
-        }).then(() => {
-          return authState;
         });
-      }).then(authState => {
-        return this.dataService.setVanity(this.signupForm.value.username, authState.uid);
       }).then(() => {
+        this.snackBar.open(`Welcome, ${this.signupForm.value.displayName}! Your account has been created.`, 'OK', {
+          duration: 5000
+        });
         this.router.navigate(['/home']);
       });
     }
-  }
-
-  private usernameValidator(control: AbstractControl): Observable<ValidationErrors | null> {
-    return this.dataService.hasVanity(control.value)
-      .map((taken: boolean) => taken ? { 'taken': true } : null)
-      .debounceTime(300)
-      .first();
   }
 }
