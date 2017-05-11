7df502b5d58c6e537f0a450abaad745d216aaa52
Tommy Jiang <tianyangj@gmail.com>
2017-05-10 23:23:35 -0700

    AppService login/logout

--- beethoven/src/app/splash/header/header.component.ts
+++ beethoven/src/app/splash/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { AppService } from '../../core/app.service';
 
 @Component({
   selector: 'lb-splash-header',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  authenticated: boolean;
+
+  constructor(
+    private angularFire: AngularFire,
+    private appService: AppService
+  ) { }
 
   ngOnInit() {
+    this.angularFire.auth.subscribe(authState => {
+      this.authenticated = authState !== null;
+    });
+  }
+
+  login() {
+    this.appService.login();
   }
 
 }
